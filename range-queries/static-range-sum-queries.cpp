a4485af793e01a0464289d93686359b7ab80b7f6
Pedro Miranda <mdeapedro@gmail.com>
2024-05-03 17:38:22 -0300

    Update Static Range Sum Queries

--- range-queries/static-range-sum-queries.cpp
+++ range-queries/static-range-sum-queries.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 void solve() {
     int n, q;
-    long long x[200001];
     cin >> n >> q;
-    x[0] = 0;
-    for (int i=1; i<=n; i++) cin >> x[i];
-    for (int i=2; i<=n; i++) x[i] += x[i-1];
-    for (int i=0; i<q; i++) {
+    vector<long long> x(n + 1);
+    for (int i = 1; i <= n; i++) cin >> x[i];
+    for (int i = 1; i <= n; i++) x[i] += x[i - 1];
+    while (q--) {
         int a, b;
         cin >> a >> b;
-        cout << x[b]-x[a-1] << '\n';
+        cout << x[b] - x[a - 1] << '\n';
     }
 }
 
