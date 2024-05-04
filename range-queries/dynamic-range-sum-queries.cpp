8ac3166b48c043f677b37c7e0681a854862d6634
Pedro Miranda <mdeapedro@gmail.com>
2024-05-03 23:00:15 -0300

    Update Dynamic Range Sum Queries

--- range-queries/dynamic-range-sum-queries.cpp
+++ range-queries/dynamic-range-sum-queries.cpp
@@ -1,50 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    int n, q;
-    cin >> n >> q;
-    vector<int> x(n + 1);
-    for (int i = 1; i <= n; i++) cin >> x[i];
-    vector<long long> segtree(4 * n + 1);
-    auto build = [&](auto self, int v, int tl, int tr) -> void {
-        if (tl == tr) {
-            segtree[v] = x[tl];
-        } else {
+struct SegmentTree {
+public:
+    SegmentTree(vector<int>& a) {
+        n = a.size();
+        segtree.assign(4 * n, 0);
+        build(a, 1, 0, n - 1);
+    }
+
+    void update(int pos, int value) {
+        update(1, 0, n - 1, pos, value);
+    }
+
+    long long query(int l, int r) {
+        return query(1, 0, n - 1, l, r);
+    }
+
+private:
+    int n;
+    vector<long long> segtree;
+
+    void build(vector<int>& a, int v, int tl, int tr) {
+        if (tl == tr) segtree[v] = a[tl];
+        else {
             int tm = (tl + tr) / 2;
-            self(self, v * 2, tl, tm);
-            self(self, v * 2 + 1, tm + 1, tr);
+            build(a, v * 2, tl, tm);
+            build(a, v * 2 + 1, tm + 1, tr);
             segtree[v] = segtree[v * 2] + segtree[v * 2 + 1];
         }
-    };
-    auto query = [&](auto self, int v, int tl, int tr, int l, int r) -> long long {
+    }
+
+    long long query(int v, int tl, int tr, int l, int r) {
         if (l > r) return 0;
         if (l == tl && r == tr) return segtree[v];
         int tm = (tl + tr) / 2;
-        return self(self, v * 2, tl, tm, l, min(r, tm)) +
-               self(self, v * 2 + 1, tm + 1, tr, max(l, tm + 1), r);
-    };
-    auto update = [&](auto self, int v, int tl, int tr, int pos, int value) -> void {
+        return query(v * 2, tl, tm, l, min(r, tm)) +
+               query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r);
+    }
+
+    void update(int v, int tl, int tr, int pos, int value) {
         if (tl == tr) segtree[v] = value;
         else {
             int tm = (tl + tr) / 2;
-            if (pos <= tm) self(self, v * 2, tl, tm, pos, value);
-            else self(self, v * 2 + 1, tm + 1, tr, pos, value);
+            if (pos <= tm) update(v * 2, tl, tm, pos, value);
+            else update(v * 2 + 1, tm + 1, tr, pos, value);
             segtree[v] = segtree[v * 2] + segtree[v * 2 + 1];
         }
-    };
-    build(build, 1, 1, n);
+    }
+};
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> x(n);
+    for (int i = 0; i < n; i++) cin >> x[i];
+    auto segtree = SegmentTree(x);
     while (q--) {
         int t;
         cin >> t;
         if (t == 1) {
             int k, u;
             cin >> k >> u;
-            update(update, 1, 1, n, k, u);
+            k--;
+            segtree.update(k, u);
         } else {
             int a, b;
             cin >> a >> b;
-            cout << query(query, 1, 1, n, a, b) << '\n';
+            a--; b--;
+            cout << segtree.query(a, b) << '\n';
         }
     }
 }
